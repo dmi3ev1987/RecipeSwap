24606e8343125c7ef45e9d183d1086941d3b7075
Andrey Dmitriev <andrey.dmi3ev@gmail.com>
2024-07-07 21:54:33 +0300

    Test 1.

--- backend/api/test.py
+++ backend/api/test.py
@@ -1,13 +1,19 @@
 from http import HTTPStatus
 
-from django.test import Client, TestCase
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from rest_framework.test import APIClient
 
 
-class FoodgramAPITestCase(TestCase):
+class CatsAPITestCase(TestCase):
     def setUp(self):
-        self.guest_client = Client()
+        User = get_user_model()
+        self.user = User.objects.create_user(username='auth_user')
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
 
     def test_list_exists(self):
-        """Проверка доступности списка рецептов."""
-        response = self.guest_client.get('/api/recipes/')
+        """Проверка доступности списка задач."""
+        response = self.client.get('/api/recipes/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
+    
