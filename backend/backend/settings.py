619e8f62ab34efa3ddee0d49d41cfdfdda7be093
Andrey Dmitriev <andrey.dmi3ev@gmail.com>
2024-07-09 12:54:55 +0300

    Settings for sqlite debug.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,6 +78,14 @@ DATABASES = {
 }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     },
+# }
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -110,6 +118,8 @@ STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/var/www/foodgram/media/'
+
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
